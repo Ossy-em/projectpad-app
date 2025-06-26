1c8c5d4945282cc2d6787949d98ab60acef1bfc1
Ossy-em <emosinachi@gmail.com>
2025-06-26 13:37:57 +0100

    installations

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+// import { Card, CardContent } from "./components/ui/card";
+
+export default function WelcomeCard() {
+  return (
+    <div> <h1 className="text-2xl font-bold">Welcome to Your Research Hub</h1>
+        <p className="text-sm text-muted-foreground mt-2">
+          Start by uploading your papers or exploring your dashboard.
+        </p></div>
+       
+    
+  );
+}
